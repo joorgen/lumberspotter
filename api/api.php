c9956fcbee4d16c6da3b470a52bedfafa8ef9a31
joorgen <sjh@mail.bg>
2015-11-22 13:56:48 +0200

    api and client

---
+++ api/api.php
@@ -0,0 +1,22 @@
+﻿<?php
+	
+	$url = '2015.json';
+	$json = file_get_contents($url);
+	$permits = json_decode($json, true);
+	// echo '<pre>';
+	// var_dump($permits);
+	// echo '</pre>';
+
+	function filterItem($item)
+	{
+	    // returns whether the input integer is odd
+	    return( $item["Община"] === "Правец" );
+	}
+
+	$permitsFiltered = array_filter($permits, "filterItem");
+	$json_output = json_encode($permitsFiltered,JSON_UNESCAPED_UNICODE);
+	//echo '<pre>';
+	print_r( $json_output );
+	//echo '</pre>';
+
+?>
