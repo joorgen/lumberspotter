3a58946c531daeac3b29d2288ee8188d6d70565c
joorgen <sjh@mail.bg>
2015-11-23 11:53:47 +0200

    Laravel freaks out on memory when decoding 20MB json file. Its an overkill, this api is just a mock for the real one.

---
+++ api/permits_api.php
@@ -0,0 +1,31 @@
+<?php
+    function filterPermits($key, $value)
+    {
+        $json = file_get_contents('2015.json');
+        $permits = json_decode($json, true);
+
+        $permitsFiltered = array();
+        foreach ($permits as &$item) {
+            if( mb_strtolower($item[$key]) === mb_strtolower($value) )
+            {
+                array_push($permitsFiltered, $item);
+            }
+        }
+        unset($item); // break the reference with the last element
+
+        return $permitsFiltered;
+    }
+	
+    if( !isset( $_GET['filters'] ))
+    {
+        exit();
+    }
+
+    $filters_json = $_GET['filters'];
+   
+    $filters = json_decode($filters_json,true);
+	$json_output = filterPermits( key($filters), $filters[key($filters)]);
+
+    header('Content-Type: application/json');
+	print_r( json_encode($json_output, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT ) );
+?>
